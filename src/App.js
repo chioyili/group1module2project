db575dff7158d8f0a3ef2a64fecff332308cb658
LaurenOng1 <laurenong96@gmail.com>
2023-10-07 20:45:20 +0800

    App and MovieSelect
    App and MovieSelect

--- src/App.js
+++ src/App.js
@@ -18,6 +18,11 @@ function App() {
   /* For apiGET */
   const [movieData, setMovieDate] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  // New states for disecting of list
+  const [mysteryMovieList, setMysteryMovieList] = useState([]);
+  const [romanceMovieList, setRomanceMovieList] = useState([]);
+  const [comedyMovieList, setComedyMovieList] = useState([]);
+  const [fullMovieList, setFullMovieList] = useState([]);
 
   const apiGet = async () => {
     try {
@@ -25,12 +30,19 @@ function App() {
       const response_mystery = await movieAPI.get(`/?apikey=628b4040&s=mystery&type=movie`);
       const response_romance = await movieAPI.get(`/?apikey=628b4040&s=romance&type=movie`);
       const response_comedy = await movieAPI.get(`/?apikey=628b4040&s=comedy&type=movie`);
+
+      setMysteryMovieList(response_mystery.data.Search);
+      setRomanceMovieList(response_romance.data.Search);
+      setComedyMovieList(response_comedy.data.Search);
+
       console.log("apiGet: data", response_mystery, response_romance, response_comedy);
       // Merge all genres to form a movie list
       const getmoviedata = [...response_mystery.data.Search, ...response_romance.data.Search, ...response_comedy.data.Search]
       // console.log("apiGet: merge movieData",getmoviedata);
 
       setMovieDate(() => getmoviedata);
+      // For full list
+      setFullMovieList(() => getmoviedata);
       // console.log("apiGet: set movieData", movieData);
       
     } catch (error) {
@@ -38,6 +50,26 @@ function App() {
     } finally {
       setIsLoading(false);
       console.log("apiGet: completed");
+    }
+  }
+
+  const selectMovieHandler = (genre) => {
+    switch(genre){
+      case 'mystery':
+        setMovieDate(mysteryMovieList)
+        break;
+        case 'romance':
+        setMovieDate(romanceMovieList)
+        break;
+        case 'comedy':
+        setMovieDate(comedyMovieList)
+        break;
+        case 'full':
+        setMovieDate(fullMovieList)
+        break;
+      default:
+        console.log('went into default...')
+        break;
     }
   }
 
@@ -52,7 +84,9 @@ function App() {
       (movieData.length === 0) && <p>No data loaded.</p>
       }
 
-      <MovieSelect movieData={movieData} />
+      <MovieSelect selectMovieHandler={selectMovieHandler}/>
+      {/* {showWhichList == 'Movie' && <MovieList movieData={movieData} />}
+      {showWhichList == 'My List' && <MyList />} */}
       <MovieList movieData={movieData} />
       <MyList />
     </div>
--- src/components/MovieSelect.js
+++ src/components/MovieSelect.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import styles from './MovieSelect.module.css'
 
-function MovieSelect({movieAPI}) {
+function MovieSelect({selectMovieHandler}) {
   return (
-    <h3>Homepage</h3>
+  <div>   
+    <h1 className={styles.header} >Movie Randomizer!</h1>
+    <h3>What do you want to watch today?</h3>
+        <button className={styles.button} onClick={() => selectMovieHandler('mystery')}>Mystery</button>         
+        <button className={styles.button} onClick={() => selectMovieHandler('romance')}>Romance</button>    
+        <button className={styles.button} onClick={() => selectMovieHandler('comedy')}>Comedy</button>     
+        <button className={styles.button} onClick={() => selectMovieHandler('full')}>All Genre</button>      
+        <br />                                                                                    
+        <button className={styles.button}>I want a random movie</button>
+        <button className={styles.button}>Give me a list</button>
+        <br />
+  </div>
   )
 }
 
