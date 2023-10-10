8a8cbbe58baba90f1f3efdf13a4d487f06b19551
Hemma <hemma1108@gmail.com>
2023-10-10 20:54:56 +0800

    cleaned up MovieList.js

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,21 +1,15 @@
 import React from "react";
-import { useState, useContext } from "react";
+import { useContext } from "react";
 
 import styles from "./MovieList.module.css";
 import HeartButton from "./HeartButtton";
 import FavListContext from "../context/FavListContext";
 
-function MovieList({
-  movieData,
-  // genre
-}) {
-  const { like, handleHeartButton, handlerAddItem } =
-    useContext(FavListContext);
+function MovieList({movieData}) {
+  const { like, handleHeartButton } = useContext(FavListContext);
 
   return (
     <div>
-      {/* <div>{genre !== '' && `Here is our top picks from ${genre} Genre!`}</div>   */}
-
       <div className={styles.movielist}>
         {movieData &&
           movieData.length > 0 &&
@@ -25,7 +19,6 @@ function MovieList({
                 {movie.Title}
                 <HeartButton
                   // checking like status of the specified movie at the given movie id (imbdID)
-                  // label={like[movie.imdbID] ? "❤️" :"♡"}
                   like={like[movie.imdbID]}
                   // when button clicked, it calles the handleHeartButton with movie.imdbID parameter (id given by the api)
                   onClick={() => handleHeartButton(movie)}
