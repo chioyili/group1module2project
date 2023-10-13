e902864b99969749964bb8c229d3c767a210a591
chioyili <chioyili@gmail.com>
2023-10-13 22:00:55 +0800

    changed api url to use https instead of http

--- src/api/moveapi.js
+++ src/api/moveapi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://www.omdbapi.com';
+const BASE_URL = 'https://www.omdbapi.com';
 const movieAPI = axios.create({baseURL: BASE_URL});
 
 export default movieAPI
